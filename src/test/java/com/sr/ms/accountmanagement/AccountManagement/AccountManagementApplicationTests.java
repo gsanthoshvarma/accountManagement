b50978892ddbe2b5b24f2222e1ccfc55b379ea3c
gsanthoshvarma <santhoshvarma.g@gmail.com>
2020-02-22 01:19:34 +0530

    inital accountManagement

---
+++ src/test/java/com/sr/ms/accountmanagement/AccountManagement/AccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.ms.accountmanagement.AccountManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
